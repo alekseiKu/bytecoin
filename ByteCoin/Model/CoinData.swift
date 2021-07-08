c7c8d47947a105df839edb04e554e1e465505e6e
12121 <kulichkin8@mail.ru>
2021-07-08 20:31:30 +0300

    first commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,15 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by 001011011 on 15.02.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    
+    let rate: Double
+    
+}
